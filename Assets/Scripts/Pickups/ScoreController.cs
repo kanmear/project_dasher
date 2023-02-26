e61b536559664f7b0c47a1d9422e78bb9538b7fe
mind_blade <kanmear+github@gmail.com>
2023-02-26 23:15:59 +0300

    feat: working score pickup

---
+++ Assets/Scripts/Pickups/ScoreController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField] GameObject _scorePickupPrefab;
+    private int _score = 0;
+    private float _maxModX;
+    private float _maxModY;
+    void Start()
+    {
+        int mapLayer = LayerMask.GetMask("Map");
+
+        float pickupRadius = _scorePickupPrefab.GetComponent<CircleCollider2D>().radius;
+        float amplitude = _scorePickupPrefab.GetComponent<ScorePickup>().getAmplitude();
+
+        _maxModX = Physics2D.Raycast(Vector2.zero, Vector2.right, float.PositiveInfinity, mapLayer).distance - pickupRadius;
+        _maxModY = Physics2D.Raycast(Vector2.zero, Vector2.up, float.PositiveInfinity, mapLayer).distance - (pickupRadius + amplitude);
+    }
+
+    void Update()
+    {
+    }
+
+    public void collectScorePickup(GameObject scorePickup)
+    {
+        _score++;
+        GameObject.Destroy(scorePickup);
+
+        Vector2 pos = new Vector2(
+            Random.Range(-_maxModX, _maxModX),
+            Random.Range(-_maxModY, _maxModY));
+        GameObject.Instantiate(_scorePickupPrefab, pos, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Pickups/ScorePickup.cs
+++ Assets/Scripts/Pickups/ScorePickup.cs
@@ -3,18 +3,35 @@ using UnityEngine;
 public class ScorePickup : MonoBehaviour
 {
 
-    [SerializeField] float _offsetMultiplier = 0.5f;
+    [SerializeField] float _amplitudeMultiplier = 0.5f;
+    [SerializeField] float _oscillationFrequency = 0.5f;
+    private ScoreController _scoreController;
     private Vector2 _startPosition;
     private Transform _transform;
+    private Collider2D _collider;
 
     void Start()
     {
+        _scoreController = GameObject.Find("ScoreController").GetComponent<ScoreController>();
         _startPosition = transform.position;
         _transform = transform;
+        _collider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        _transform.position = _startPosition + new Vector2(0f, Mathf.Sin(Time.time) * _offsetMultiplier);
+        _transform.position = _startPosition + new Vector2(
+            0f, Mathf.Sin(Time.time * _oscillationFrequency) * _amplitudeMultiplier);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        _scoreController.collectScorePickup(gameObject);
+        Debug.Log(collider.gameObject);
+    }
+
+    public float getAmplitude()
+    {
+        return _amplitudeMultiplier;
     }
 }
